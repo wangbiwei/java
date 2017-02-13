40bc854e6b7965ffe667e0f039ef61da4522a130
WangBiWei <weiyvhu@foxmail.com>
2017-02-13 23:41:05 +0800

    【description】：遍历文件夹，搜索文件夹特定文件，新建文件夹或者文件 【date】：2017年2月13日23:40:52 【提交人】：王必伟 【其他】：

--- javaIO/src/wang/w2017年2月9日/File/FileStudy.java
+++ javaIO/src/wang/w2017年2月9日/File/FileStudy.java
@@ -5,41 +5,83 @@ package wang.w2017年2月9日.File;
 
 
 import java.io.File;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+import java.util.regex.Pattern;
+
+import 工具.时间;
+import 常量.真假;
+import 常量.返回对象;
 
 
 /**
  * @author 王必伟
  *
- * @date 2017年2月9日
+ * @date 2017年2月13日
  *
  * @tags
  */
 public class FileStudy
 {
 	/**
-	 * 遍历文件夹
-	 * 
-	 * @param file 要遍历的文件夹
+	 * 新建文件夹或者文件
+	 * @param file 要建的文件夹或者文件
+	 * @param FileOrDirectory 标志
+	 * @return
 	 */
-	public static void 遍历文件夹(File file)
+	public static 返回对象 createFileOrDirectory(String path, String FileOrDirectory)
 	{
-		File[] list = file.listFiles();
-		if (list == null) {
-			System.err.println(file.getAbsolutePath());
+		final String FILE = "F";
+		final String DIRECTORY = "D";
+		File file = new File(path);
+		if (file.exists()) {
+			String eoerMsg;
+			if (FILE.equalsIgnoreCase(FileOrDirectory)) {
+				eoerMsg = "文件已经存在";
+			} else {
+				eoerMsg = "文件夹已经存在";
+			}
+			return new 返回对象(真假.FLASE, eoerMsg);
+		}
+		if (DIRECTORY.equalsIgnoreCase(FileOrDirectory)) {
+			if (file.mkdirs()) {
+				return new 返回对象(真假.TRUE, "文件夹创建成功");
+			} else {
+				return new 返回对象(真假.TRUE, "文件夹创建失败");
+			}
+		} else {
 			try {
-				Thread.sleep(5000);// 让主线程休眠10秒，看哪些文件夹不可访问；
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-			return;
-		}
-		for (File item : list) {
-			if (item.isFile()) {
-				System.out.println(getInfo(item));
-			} else if (item.isDirectory()) {
-				遍历文件夹(item);
+				file.createNewFile();
+				return new 返回对象(真假.TRUE, "文件创建成功");
+			} catch (IOException e) {
+				return new 返回对象(真假.TRUE, "文件创建失败");
 			}
 		}
+	}
+	
+	/**
+	 * 类似Everything的功能
+	 * @param regex
+	 * @return
+	 */
+	public static 返回对象 类似Everything(String regex)
+	{
+		List<List<File>> re = new ArrayList<>();
+		
+		List<File> result = new ArrayList<File>();
+		List<File> errorResult = new ArrayList<>();
+		File[] 盘符 = File.listRoots();
+		for (File file : 盘符) {
+			@SuppressWarnings("unchecked")
+			List<List<File>> temp = (List<List<File>>) 选择某一路径下满足某些条件的文件(file, regex).getObj();
+			result.addAll(temp.get(0));
+			errorResult.addAll(temp.get(1));
+		}
+		return new 返回对象(真假.TRUE, re);
 	}
 	
 	/**
@@ -47,9 +89,100 @@ public class FileStudy
 	 * 
 	 * @param path 文件夹路径
 	 */
-	public static void 遍历文件夹(String path)
+	public static 返回对象 遍历文件夹(String path)
 	{
-		遍历文件夹(new File(path));
+		return 遍历文件夹(new File(path));
+	}
+	
+	/**
+	 * 选择某一路径下满足某些条件的文件（满足正则表达式）
+	 * @param path
+	 * @param regex 搜索条件
+	 * @return
+	 */
+	public static 返回对象 选择某一路径下满足某些条件的文件(String path, String regex)
+	{
+		return 选择某一路径下满足某些条件的文件(new File(path), regex);
+	}
+	
+	/**
+	 * 遍历文件夹
+	 * 
+	 * @param file 要遍历的文件夹
+	 */
+	@SuppressWarnings("unchecked")
+	public static 返回对象 遍历文件夹(File file)
+	{
+		List<List<File>> re = new ArrayList<>();
+		
+		List<File> result = new ArrayList<File>();
+		List<File> errorResult = new ArrayList<>();
+		re.add(result);
+		re.add(errorResult);
+		
+		File[] list = file.listFiles();
+		if (list == null) {// 文件不存在或者不能访问
+			errorResult.add(file);
+			/*
+			 * try { Thread.sleep(5000);// 让主线程休眠10秒，看哪些文件夹不可访问； } catch
+			 * (InterruptedException e) { e.printStackTrace(); }
+			 */
+			return new 返回对象(真假.TRUE, re);
+		}
+		for (File item : list) {
+			if (item.isFile()) {
+				result.add(item);
+			} else if (item.isDirectory()) {
+				result.addAll((Collection<? extends File>) ((List<List<File>>) 遍历文件夹(item).getObj()).get(0));
+				errorResult.addAll((Collection<? extends File>) ((List<List<File>>) 遍历文件夹(item).getObj()).get(1));
+			}
+		}
+		return new 返回对象(真假.TRUE, re);
+	}
+	
+	/**
+	 * 选择某一路径下满足某些条件的文件（满足正则表达式）
+	 * @param file
+	 * @param regex 搜索条件
+	 * @return
+	 */
+	public static 返回对象 选择某一路径下满足某些条件的文件(File file, String regex)
+	{
+		List<List<File>> re = new ArrayList<>();
+		
+		List<File> result = new ArrayList<File>();
+		List<File> errorResult = new ArrayList<>();// 装的是不能访问或者是不存在的文件
+		re.add(result);
+		re.add(errorResult);
+		File[] list = file.listFiles();
+		File[] selectList = file.listFiles(new FilenameFilter() {
+			
+			@Override
+			public boolean accept(File dir, String name)
+			{
+				return Pattern.compile(tanslateString(regex)).matcher(name).find();
+			}
+			
+		});
+		if (null == list) {// 文件不存在或者不能访问
+			errorResult.add(file);
+			System.out.println(file.getPath());
+			return new 返回对象(真假.FLASE, re);
+		}
+		for (File item : selectList) {
+			if (item.isFile()) {
+				result.add(item);
+			}
+		}
+		for (File item : list) {
+			if (item.isDirectory()) {
+				@SuppressWarnings("unchecked")
+				List<List<File>> temp = (List<List<File>>) 选择某一路径下满足某些条件的文件(item, regex).getObj();
+				result.addAll(temp.get(0));
+				errorResult.addAll(temp.get(1));
+			}
+		}
+		return new 返回对象(真假.TRUE, re);
 	}
 	
 	/**
@@ -58,12 +191,13 @@ public class FileStudy
 	 * @param item 文件
 	 * @return 上诉文件信息；
 	 */
-	private static String getInfo(File item)
+	public static String getInfo(File item)
 	{
 		String fileName = item.getName();
 		String fileSize = getFileSize(item.length());
 		String filePath = item.getParent();
-		return fileName + " " + fileSize + " " + filePath;
+		String lastModified = 时间.getSdf().format(new Date(item.lastModified()));
+		return fileName + " " + fileSize + " " + filePath + "  " + lastModified;
 	}
 	
 	/**
@@ -94,7 +228,11 @@ public class FileStudy
 		}
 		int remainder = (int) (size % 1024);
 		long quotient = size / 1024;
-		return 递归求余数(quotient, 单位, index++) + remainder + 单位[index];
+		return 递归求余数(quotient, 单位, index + 1) + remainder + 单位[index];
 	}
 	
+	private static String tanslateString(String regex)
+	{
+		return regex.replace("*", "[\\s\\S]*");
+	}
 }
